2bb4708a9320929e176c0f7bc1603eb6675bf8e0
vijeth <vijeth.7mar@gmail.com>
2018-09-23 12:43:56 +0530

    A sample Spring-Boot project with Swagger

--- swagger-sample/src/main/java/com/vijeth/sample/ApplicationMain.java
+++ swagger-sample/src/main/java/com/vijeth/sample/ApplicationMain.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "com.vijeth.sample")
-public class ApplicationMain {
+public class  ApplicationMain {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationMain.class);
     }
